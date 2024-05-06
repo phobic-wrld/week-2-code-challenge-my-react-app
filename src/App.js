9dd9d6c7bda8ec04ad1a5b852aeacc50c181b5a1
joy <blackbearmode1@gmail.com>
2024-05-06 11:56:40 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import BotCollection from './BotCollection';
-import YourBotArmy from './YourBotArmy';
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
-  const [bots, setBots] = useState([]);
-  const [army, setArmy] = useState([]);
-
-  useEffect(() => {
-    // Fetch data about available bots
-    // Update the state with the fetched data
-  }, []);
-
-  const enlistBot = (bot) => {
-    // Implement enlist functionality
-  };
-
-  const releaseBot = (bot) => {
-    setArmy(army.filter(b => b.id !== bot.id));
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Bot Battlr</h1>
-      <BotCollection bots={bots} enlistBot={enlistBot} />
-      <YourBotArmy army={army} releaseBot={releaseBot} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import routes from './routes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter(routes);
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
-// root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
